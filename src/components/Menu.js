e1d72eed230f4e58b0d99797e03e16b320fad7a9
Jesse Wright <jessewright01@gmail.com>
2018-06-27 18:00:12 -0400

    yeah

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,62 +4,47 @@ import styled from 'styled-components'
 import logo from '../images/logo.svg'
 import Headroom from 'react-headroom'
 
-const Header = styled.header`
+const HeaderFullWidth = styled.header`
   background: ${props => props.theme.colors.base};
   width: 100%;
-  padding: .8em 0;
 `
-const Nav = styled.nav`
-  width: 100%;
+
+const Header = styled.div`
   max-width: ${props => props.theme.sizes.maxWidth};
-  margin: 0 auto;
-  padding: 0 1.5em;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  flex-direction: row;
 
-  ul {
-    
+  img {
+    height: 2em;
+    width: auto;
+    margin: 0em .6em;
   }
+`
 
-  li {
-    display: inline-block;
-    margin-left: 1em;
-    &:first-child {
-      margin-left: 0;
-      flex-basis: 100%;
-    }
-  }
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+
   a {
     text-decoration: none;
     color: white;
     font-weight: 400;
     transition: all 0.2s;
+    padding: 1em .5em;
   }
-  img {
-    height: 2em;
-    width: auto;
-  }
+
   @media (max-width: ${props => props.theme.responsive.small}) {
-    ul {
-      mask-image: -webkit-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      mask-image: -moz-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      mask-image: linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      -webkit-mask-image: -webkit-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      -webkit-mask-image: -moz-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      -webkit-mask-image: linear-gradient(to right, transparent, black 15px, black 90%, transparent);
-      overflow: auto;
-      white-space: nowrap;
-      width: 100%;
-      padding: 0em .6em;
-    }
-
-    img {
-      margin-left: -.75em;
-      margin-right: .5em;
-    }
-    
-
+    mask-image: -webkit-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    mask-image: -moz-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    mask-image: linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    -webkit-mask-image: -webkit-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    -webkit-mask-image: -moz-linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    -webkit-mask-image: linear-gradient(to right, transparent, black 15px, black 90%, transparent);
+    overflow: auto;
+    white-space: nowrap;
   }
 `
 
@@ -70,47 +55,34 @@ const activeLinkStyle = {
 const Menu = () => {
   return (
     <Headroom>
-    <Header>
-      <Nav>
-
+      <HeaderFullWidth>
+        <Header>
           <Link to="/" exact activeStyle={activeLinkStyle}>
             <img src={logo} alt="Wright Mowers Logo"/>
           </Link>
 
-          <ul>
-            <li>
-              <Link to="/stand-on/" activeStyle={activeLinkStyle}>
-                Stand
-              </Link>
-            </li>
-            <li>
-              <Link to="/walk-behind/" activeStyle={activeLinkStyle}>
-                Ride
-              </Link>
-            </li>
-            <li>
-              <Link to="/zero-turn/" activeStyle={activeLinkStyle}>
-                Walk
-              </Link>
-            </li>
-            <li>
-              <Link to="/accessories/" activeStyle={activeLinkStyle}>
-                Accessories
-              </Link>
-            </li>
-            <li>
-              <Link to="/parts-search/" activeStyle={activeLinkStyle}>
-                Support
-              </Link>
-            </li>
-            <li>
-              <Link to="/find-dealer/" activeStyle={activeLinkStyle}>
-                Retailers
-              </Link>
-            </li>
-          </ul>
-        </Nav>
-    </Header>
+          <Nav>
+            <Link to="/stand-on/" activeStyle={activeLinkStyle}>
+              Stand
+            </Link>
+            <Link to="/walk-behind/" activeStyle={activeLinkStyle}>
+              Ride
+            </Link>
+            <Link to="/zero-turn/" activeStyle={activeLinkStyle}>
+              Walk
+            </Link>
+            <Link to="/accessories/" activeStyle={activeLinkStyle}>
+              Accessories
+            </Link>
+            <Link to="/parts-search/" activeStyle={activeLinkStyle}>
+              Support
+            </Link>
+            <Link to="/find-dealer/" activeStyle={activeLinkStyle}>
+              Find Dealer
+            </Link>
+          </Nav>
+        </Header>
+      </HeaderFullWidth>
     </Headroom>
   )
 }
