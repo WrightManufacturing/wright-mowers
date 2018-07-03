247fc5025151da916b2c72f0b106ac46865f581a
Jesse Wright <jessewright01@gmail.com>
2018-07-03 15:08:05 -0400

    updates

--- src/pages/find-dealer.js
+++ src/pages/find-dealer.js
@@ -4,13 +4,15 @@ import PageTitle from '../components/PageTitle'
 import SEO from '../components/SEO'
 import styled from 'styled-components'
 
-import ReactMapGL from 'react-map-gl';
+import GeoLocation from '../components/GeoLocation'
+import ReactMapGL, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MapContainer = styled.div`
   margin: auto;
   padding: auto;
 `
+
 
 class DealerLocator extends React.Component {
   state = {
@@ -32,16 +34,24 @@ class DealerLocator extends React.Component {
           <MapContainer>
             <ReactMapGL
               mapboxApiAccessToken={'pk.eyJ1IjoiamVzc2V3cmlnaHQiLCJhIjoiY2pqMDdja3NuMGV2YTNvcGI5ZWx6MDBoZiJ9.6Rp8KPlFDyhX9_WJIyze1w'}
+              latitude={37.78}
+              longitude={-122.41}
               width={400}
               height={400}
-              latitude={37.7577}
-              longitude={-122.4376}
-              zoom={8}
+              zoom={7}
               {...this.state.viewport}
               onViewportChange={(viewport) => this.setState({viewport})}
             >
+            <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
+              <div style={{color: '#fdbb2d', fontWeight: 'bold' }}>DEALER</div>
+            </Marker>
+            <Marker latitude={37.50} longitude={-122.30} offsetLeft={-20} offsetTop={-10}>
+              <div style={{color: '#fdbb2d', fontWeight: 'bold' }}>DEALER</div>
+            </Marker>
             </ReactMapGL>
           </MapContainer>
+
+          <GeoLocation />
 
 
         </Container>
